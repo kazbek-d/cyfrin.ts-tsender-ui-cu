a9fdee3e6f045a196c1b04af41a47e2c6f932351
Anzhela DZARASOVA <angelas82@list.ru>
2026-03-09 08:40:28 +0100

    Usestate and Hooks

--- ts-tsender-ui/src/components/AirdropForm.tsx
+++ ts-tsender-ui/src/components/AirdropForm.tsx
@@ -5,6 +5,14 @@ import { useState } from "react"
 
 export default function AirdropForm() {
     const [tokenAddress, setTokenAddress] = useState("")
+    const [recipients, setRecipients] = useState("")
+    const [amount, setAmount] = useState("")
+
+    async function handleSubmit() {
+        console.log("tokenAddress", tokenAddress)
+        console.log("recipients", recipients)
+        console.log("amount", amount)
+    }
 
     return (
         <div>
@@ -14,6 +22,25 @@ export default function AirdropForm() {
                 value={tokenAddress}
                 onChange={e => setTokenAddress(e.target.value)}
             />
+            <InputForm
+                label="Recipients"
+                placeholder="0x123,0x1234,0x12345,..."
+                value={recipients}
+                onChange={e => setRecipients(e.target.value)}
+                large={true}
+            />
+            <InputForm
+                label="Amount"
+                placeholder="100,200,300,..."
+                value={amount}
+                onChange={e => setAmount(e.target.value)}
+                large={true}
+            />
+            <button
+                onClick={handleSubmit}
+            >
+                Send Tokens
+            </button>
         </div>
     )
 }
