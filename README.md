5924b3f8f721a54f134c4c71bbd89babaa4dd3bf
Anzhela DZARASOVA <angelas82@list.ru>
2026-03-16 18:50:27 +0100

    Added formatTokenAmount Utils, Persist and ReadContracts

--- README.md
+++ README.md
@@ -49,6 +49,8 @@ pnpm anvil
 -> Token Address: 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512
 -> Recipients: 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
 -> Amount: 1
+
+-> Recipients: 0x9965507D1a55bcC2695C58ba16FB37d819B0A4dc
 ```
 
 
--- ts-tsender-ui/src/components/AirdropForm.tsx
+++ ts-tsender-ui/src/components/AirdropForm.tsx
@@ -1,20 +1,89 @@
 "use client"
 
 import { InputForm } from "@/components/ui/InputField"
-import { useState, useMemo } from "react"
-import { chainsToTSender, erc20Abi, tsenderAbi } from "@/constants"
-import { useChainId, useConfig, useConnection } from "wagmi"
-import { readContract } from "@wagmi/core"
-import { calculateTotal } from "@/utils"
+import {
+    useState,
+    useMemo,
+    useEffect
+} from "react"
+import {
+    chainsToTSender,
+    erc20Abi,
+    tsenderAbi
+} from "@/constants"
+import {
+    useChainId,
+    useConfig,
+    useConnection,
+    useWriteContract,
+    useWaitForTransactionReceipt,
+    useReadContracts
+} from "wagmi"
+import {
+    readContract,
+    waitForTransactionReceipt
+} from "@wagmi/core"
+import { calculateTotal, formatTokenAmount } from "@/utils"
 
 export default function AirdropForm() {
-    const [tokenAddress, setTokenAddress] = useState("")
-    const [recipients, setRecipients] = useState("")
-    const [amounts, setAmounts] = useState("")
+
     const chainId = useChainId()
     const config = useConfig()
     const connection = useConnection()
+    const { data: hash, isPending, error, mutateAsync } = useWriteContract()
+    const { isLoading: isConfirming, isSuccess: isConfirmed, isError } = useWaitForTransactionReceipt({
+        confirmations: 1,
+        hash,
+    })
+
+    // #region amounts, recipients and tokenAddress
+    // State
+    const [tokenAddress, setTokenAddress] = useState("")
+    const [recipients, setRecipients] = useState("")
+    const [amounts, setAmounts] = useState("")
+
+    // Load State from localStorage
+    useEffect(() => {
+        const savedTokenAddress = localStorage.getItem('tokenAddress')
+        const savedRecipients = localStorage.getItem('recipients')
+        const savedAmounts = localStorage.getItem('amounts')
+
+        if (savedTokenAddress) setTokenAddress(savedTokenAddress)
+        if (savedRecipients) setRecipients(savedRecipients)
+        if (savedAmounts) setAmounts(savedAmounts)
+    }, []);
+
+    // Save State to localStorage
+    useEffect(() => localStorage.setItem('tokenAddress', tokenAddress), [tokenAddress]);
+    useEffect(() => localStorage.setItem('recipients', recipients), [recipients]);
+    useEffect(() => localStorage.setItem('amounts', amounts), [amounts]);
+    // #endregion
+
+    const { data: tokenData } = useReadContracts({
+        contracts: [
+            {
+                abi: erc20Abi,
+                address: tokenAddress as `0x${string}`,
+                functionName: "decimals",
+            },
+            {
+                abi: erc20Abi,
+                address: tokenAddress as `0x${string}`,
+                functionName: "name",
+            },
+            {
+                abi: erc20Abi,
+                address: tokenAddress as `0x${string}`,
+                functionName: "balanceOf",
+                args: [connection.address],
+            },
+        ]
+    })
+
+
     const total: number = useMemo(() => calculateTotal(amounts), [amounts])
+
+
 
     async function getApprovedAmount(tSenderAddress: string | null): Promise<number> {
         if (!tSenderAddress) {
@@ -44,8 +113,45 @@ export default function AirdropForm() {
         console.log("tSenderAddress", tSenderAddress)
         console.log("approvedAmount", approvedAmount)
 
+        if (approvedAmount < total) {
+            const approvalHash = await mutateAsync({
+                abi: erc20Abi,
+                address: tokenAddress as `0x${string}`,
+                functionName: 'approve',
+                args: [tSenderAddress as `0x${string}`, BigInt(total)]
+            })
+            const approvalReceipt = await waitForTransactionReceipt(config, {
+                hash: approvalHash
+            })
+            console.log("Approval confirmed: ", approvalReceipt)
 
+            await mutateAsync({
+                abi: tsenderAbi,
+                address: tSenderAddress as `0x${string}`,
+                functionName: 'airdropERC20',
+                args: [
+                    tokenAddress,
+                    recipients.split(/[,\n]+/).map(addr => addr.trim()).filter(addr => addr !== ''),
+                    amounts.split(/[,\n]+/).map(amt => amt.trim()).filter(amt => amt !== ''),
+                    BigInt(total),
+                ]
+            })
+        } else {
+            await mutateAsync({
+                abi: tsenderAbi,
+                address: tSenderAddress as `0x${string}`,
+                functionName: 'airdropERC20',
+                args: [
+                    tokenAddress,
+                    recipients.split(/[,\n]+/).map(addr => addr.trim()).filter(addr => addr !== ''),
+                    amounts.split(/[,\n]+/).map(amt => amt.trim()).filter(amt => amt !== ''),
+                    BigInt(total),
+                ]
+            })
+        }
     }
+
+
 
     return (
         <div>
@@ -69,6 +175,31 @@ export default function AirdropForm() {
                 onChange={e => setAmounts(e.target.value)}
                 large={true}
             />
+            <div className="bg-white border border-zinc-300 rounded-lg p-4">
+                <h3 className="text-sm font-medium text-zinc-900 mb-3">Transaction Details</h3>
+                <div className="space-y-2">
+                    <div className="flex justify-between items-center">
+                        <span className="text-sm text-zinc-600">Token Name:</span>
+                        <span className="font-mono text-zinc-900">
+                            {
+                                tokenData?.[1]?.result as string
+                            }
+                        </span>
+                    </div>
+                    <div className="flex justify-between items-center">
+                        <span className="text-sm text-zinc-600">Amount (wei):</span>
+                        <span className="font-mono text-zinc-900">{total}</span>
+                    </div>
+                    <div className="flex justify-between items-center">
+                        <span className="text-sm text-zinc-600">Amount (tokens):</span>
+                        <span className="font-mono text-zinc-900">
+                            {
+                                formatTokenAmount(total, tokenData?.[0]?.result as number)
+                            }
+                        </span>
+                    </div>
+                </div>
+            </div>
             <button
                 onClick={handleSubmit}
                 className="
